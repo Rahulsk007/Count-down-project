9ce11ea45ccf4822d565026c025a333d9dea6474
Rahulsk007 <rsk190102@gmail.com>
2024-06-25 13:43:09 +0530

    this is count-down-project

---
+++ countdown.js
@@ -0,0 +1,26 @@
+const endDate = "30 JUNE 2024 10:00 AM";
+
+const input = document.querySelectorAll("input");
+
+document.querySelector("#end-date").innerHTML = endDate;
+
+const clock = () =>{
+        const end = new Date(endDate);
+
+        const now = new Date();
+
+        
+        const diff =( end - now) /1000;
+
+      input[0].value =  Math.floor(diff / 3600 / 24);
+      input[1].value =  Math.floor((diff / 3600 ) % 24);
+      input[2].value =  Math.floor((diff / 60 ) % 60);
+      input[3].value =  Math.floor(diff) % 60;
+
+}
+clock();
+
+setInterval(() => {
+    clock();
+}, 1000);
+
